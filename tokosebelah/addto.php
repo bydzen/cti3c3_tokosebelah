36c0d91d4618832bc7e1fc424feed6942909c041
Sultan Kautsar <42274355+bydzen@users.noreply.github.com>
2022-01-17 10:10:11 +0700

    Create exception login user ⚙️

---
+++ tokosebelah/addto.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<center>
+<h1>Please login first!</h1>
+<a href="https://rayatiga.com/tokosebelah/User/login.php"><p>Login here.</p></a>
+</center>
+
+</body>
+</html>
